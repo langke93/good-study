b63f15248bca12cf39d8eb5ecd806db4b7ec7fa4
zhuangzhaoli <zhuangzhaoli@cyou-inc.com>
2020-04-28 18:44:11 +0800

    Jackson版本升级，dubbo上下文获取远程ip

--- dubbo-rest/dubbo-rest-service/src/main/java/org/langke/dubbo/rest/service/UserServiceImpl.java
+++ dubbo-rest/dubbo-rest-service/src/main/java/org/langke/dubbo/rest/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.langke.dubbo.rest.service;
 
+import org.apache.dubbo.rpc.RpcContext;
 import org.langke.dubbo.rest.bean.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +10,13 @@ public class UserServiceImpl implements UserService{
     Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @Override
     public Boolean registerUser(User user) {
-        logger.info("registerUser:{}",user.getUserName());
+        logger.info("registerUser:{} ip:{}",user.getUserName(),RpcContext.getContext().getRemoteAddressString());
         return true;
     }
 
     @Override
     public User getUser(Long id) {
-        logger.info("getUser:{}",id);
+        logger.info("getUser:{} ip:{}",id, RpcContext.getContext().getRemoteAddressString());
         User user = new User();
         user.setUserName("Hello Name 用户名");
         user.setId(id);
